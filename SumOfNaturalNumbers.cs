1366cf4eb4b5a42145b8e7158d07a6ce4b5bd37e
Abdullah Ranjha <abdullahranjha885@gmail.com>
2025-10-05 16:22:44 +0500

    Add files via upload

--- SumOfNaturalNumbers.cs
+++ SumOfNaturalNumbers.cs
@@ -1,17 +1,17 @@
-﻿using System;
+﻿//using System;
 
 
-	class SumOfNaturalNumbers
-	{
-    static void Main()
-    {
-        Console.Write("Enter the number (n) :");
-        int n = Convert.ToInt32(Console.ReadLine());
-        int sum = 0;
-        for (int i = 0; i <= n; i++)
-        {
-          sum = sum + i;
-        }
-        Console.WriteLine($"The sum of numbers from 1 to {n} is : {sum}");
-    }
-	}
+//	class SumOfNaturalNumbers
+//	{
+//    static void Main()
+//    {
+//        Console.Write("Enter the number (n) :");
+//        int n = Convert.ToInt32(Console.ReadLine());
+//        int sum = 0;
+//        for (int i = 0; i <= n; i++)
+//        {
+//          sum = sum + i;
+//        }
+//        Console.WriteLine($"The sum of numbers from 1 to {n} is : {sum}");
+//    }
+//	}
